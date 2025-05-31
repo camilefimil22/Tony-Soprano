2fda5129ddd11fb1f4cd4e1a4517ffcb1104fd6b
camilefimil22 <camilalefimil@hotmail.com>
2025-05-30 21:53:20 -0300

    imagenes terapia

--- src/Terapia.tsx
+++ src/Terapia.tsx
@@ -1,8 +1,12 @@
+import doctora from '../src/assets/dr.jpg'
+
 export default function Terapia(){
  return (
     <>
-  
-       <div className= "bg-[url(./src/assets/dr.jpg)] relative bg-cover bg-center h-screen">
+    
+    <img className="relative bg-cover bg-center h-screen" src={doctora}></img>
+    <div className= "px-4 sm:px-6 lg:px-16 max-w-5xl mx-auto">
+    
      < div className= "absolute h-50 w-150 left-153 top-75 inset-0  bg-opacity-60 flex flex-col items-center justify-center text-center px-6">
      <h1 className= "text-white text-3xl md:text-5xl font-bold leading-right mb-4"> Tony soprano y la terapia conductual</h1>
      </div>
@@ -12,59 +16,58 @@ export default function Terapia(){
 
 <div className='flex gap-60 text-center text-xl font-(family-name:--Roboto) text-black mx-auto mb-4 mt-10' > 
 Desde su infancia, Tony ha sufrido ataques de pánico que a veces le hacen perder el conocimiento. 
-Tiene su primer ataque de pánico en pantalla mientras cocinaba salchichas en la fiesta de cumpleaños de su hijo, pierde el conocimiento y provoca una pequeña explosión cuando 
-deja caer una botella de líquido para encendedores sobre las brasas. 
-Tony describe la experiencia del ataque de pánico como una sensación como si tuviera "ginger ale en el cráneo". Esto lo impulsa a buscar ayuda para los ataques. 
-Después de pruebas exhaustivas que incluyen una resonancia magnética y análisis de sangre, no se puede encontrar una causa física, por lo que el Dr. Cusamano remitió a Tony a 
-la psiquiatra, la Dra. Jennifer Melfi.
-
-Al principio, Tony se resistía mucho a la idea de que hubiera una causa psiquiátrica para sus síntomas. Le molestaba estar en terapia y se negó a aceptar el diagnóstico de
-ataques de pánico que le habían dado los neurólogos que habían investigado su enfermedad. Tony comienza a abrirse una vez que el Dr. Melfi explica las reglas de
-confidencialidad médico-paciente. Él le cuenta sobre el estrés de su vida de negocios: tiene la sensación de que ha llegado al final de algo y describe una reverencia por
-los "viejos tiempos" glorificados de la mafia. Tony deja de lado la violencia asociada con su carrera criminal. Tony le cuenta al Dr. Melfi una historia sobre patos que
-aterrizan en su piscina. También le habla de su madre, Livia, una mujer fría y malvada con la que tiene una relación abiertamente hostil. Al final de la primera sesión,
-Tony ha admitido que se siente deprimido. Melfi le receta Prozac como antidepresivo, diciéndole que nadie tiene por qué sufrir depresión con las maravillas de la farmacología moderna. 
+Tiene su primer ataque de pánico en pantalla mientras cocinaba salchichas en la fiesta de cumpleaños de su hijo, pierde el 
+conocimiento y provoca una pequeña explosión cuando deja caer una botella de líquido para encendedores sobre las brasas. 
+Tony describe la experiencia del ataque de pánico como una sensación como si tuviera "ginger ale en el cráneo". Esto lo impulsa a 
+buscar ayuda para los ataques. 
+Después de pruebas exhaustivas que incluyen una resonancia magnética y análisis de sangre, no se puede encontrar una causa física,
+por lo que el Dr. Cusamano remitió a Tony a la psiquiatra, la Dra. Jennifer Melfi.
+Al principio, Tony se resistía mucho a la idea de que hubiera una causa psiquiátrica para sus síntomas. Le molestaba estar en
+terapia y se negó a aceptar el diagnóstico de ataques de pánico que le habían dado los neurólogos que habían investigado su 
+enfermedad. Tony comienza a abrirse una vez que el Dr. Melfi explica las reglas de confidencialidad médico-paciente. Él le cuenta 
+sobre el estrés de su vida de negocios: tiene la sensación de que ha llegado al final de algo y describe una reverencia por los 
+"viejos tiempos" glorificados de la mafia. Tony deja de lado la violencia asociada con su carrera criminal. Tony le cuenta a la 
+Dr. Melfi una historia sobre patos que aterrizan en su piscina. También le habla de su madre, Livia, una mujer fría y malvada con la
+que tiene una relación abiertamente hostil. Al final de la primera sesión, Tony ha admitido que se siente deprimido. Melfi le 
+receta Prozac como antidepresivo, diciéndole que nadie tiene por qué sufrir depresión con las maravillas de la farmacología moderna. 
 </div>
 
  <div className="bg-[url(./src/assets/melfi3.jfif)] bg-center h-90 w-150 mx-auto mb-4 mt-10" />
 
 
 <div className='flex gap-60 text-center text-xl font-(family-name:--Roboto) text-black'> 
-
-En su próxima sesión, Tony todavía se muestra reacio a enfrentar sus propias debilidades psicológicas. Tony atribuye a la medicación la mejora de su estado de
-ánimo, pero la Dra. Melfi le dice que no puede ser eso, ya que tarda 6 semanas en hacer efecto, y ella da crédito a sus sesiones de terapia. Tony describe un sueño en el que
-un pájaro le roba el pene: Melfi extrapola de esto para revelar que Tony proyectó su amor por su familia en los patos que viven en su piscina trasera y esto lo hace
-llorar, para su consternación. Ella le dice que su huida de la piscina provocó su ataque de pánico a través del miedo abrumador de perder de alguna manera a su propia familia.
-
-Tony admite que se siente culpable porque a su madre no se le permitió 
-vivir con su familia. Nos enteramos de que sus hermanas lo han dejado solo para cuidar de su madre. Cuando el Dr. Melfi le pide que recuerde las buenas experiencias de su infancia, 
-tiene dificultades. También muestra que culpa a Carmela por impedir que su madre viva con ellos. Más tarde, discuten el accidente automovilístico de Livia y Melfi sugiere que la
-depresión puede haber contribuido al accidente: Tony la malinterpreta y se enoja. Tony tiene un ataque de pánico mientras visita la casa de su madre después de que ella se
-muda a Green Grove. En una sesión posterior, el Dr. Melfi presiona a Tony para que admita que tiene sentimientos de ira hacia su madre y nuevamente se va. Durante este 
-episodio, Tony introduce el concepto de que él actúa como el payaso triste: feliz por fuera pero triste por dentro.
-
+En su próxima sesión, Tony todavía se muestra reacio a enfrentar sus propias debilidades psicológicas. Tony atribuye a la medicación
+la mejora de su estado de ánimo, pero la Dra. Melfi le dice que no puede ser eso, ya que tarda 6 semanas en hacer efecto, y ella da
+crédito a sus sesiones de terapia. Tony describe un sueño en el que un pájaro le roba el pene: Melfi extrapola de esto para revelar 
+que Tony proyectó su amor por su familia en los patos que viven en su piscina trasera y esto lo hace llorar, para su consternación. 
+Ella le dice que su huida de la piscina provocó su ataque de pánico a través del miedo abrumador de perder de alguna manera a su 
+propia familia.
+Tony admite que se siente culpable porque a su madre no se le permitió vivir con su familia. Nos enteramos de que sus hermanas lo
+han dejado solo para cuidar de su madre. Cuando el Dr. Melfi le pide que recuerde las buenas experiencias de su infancia, 
+tiene dificultades. También muestra que culpa a Carmela por impedir que su madre viva con ellos. Más tarde, discuten el accidente 
+automovilístico de Livia y Melfi sugiere que la depresión puede haber contribuido al accidente: Tony la malinterpreta y se enoja.
+Tony tiene un ataque de pánico mientras visita la casa de su madre después de que ella se muda a Green Grove. 
+En una sesión posterior, el Dr. Melfi presiona a Tony para que admita que tiene sentimientos de ira hacia su madre y nuevamente 
+se va. Durante este  episodio, Tony introduce el concepto de que él actúa como el payaso triste: feliz por fuera pero triste por dentro.
 </div>
+
+
 <div className="bg-[url(./src/assets/melfi1.jfif)] bg-center h-90 w-150 mx-auto mb-4 mt-10" />
 
 <div className='flex gap-60 text-center text-xl font-(family-name:--Roboto) text-black'> 
-
 En "Negación, ira, aceptación", Tony discute el cáncer de Jackie con la Dr. Melfi. Ella trata de usarlo como un ejemplo de los pensamientos negativos de Tony que contribuyen a 
 su depresión. Tony se enoja y sale furioso porque siente que ella está tratando de engañarlo y manipular sus pensamientos usando las imágenes que decoran su oficina. Después 
 de que Jackie empeora y Tony es llamado Frankenstein por un socio comercial, regresa a terapia para discutir estas cosas con la Dra. Melfi, ella le pregunta si se siente como 
 un monstruo.
-
 En "Fortunate Son", Tony habla de un recuerdo de la infancia de un ataque de pánico temprano. Vio a su padre y a su tío mutilar al señor Satriale, el carnicero local, y más 
 tarde se desmayó en una cena familiar hecha con carne gratis de la tienda de Satriale. El Dr. Melfi hace una conexión entre la carne y los ataques de pánico de Tony y también 
 explora la actitud de su madre hacia los frutos del trabajo de su padre.
-
 Más tarde, la Dr. Melfi intenta recetar litio como estabilizador del estado de ánimo. En el episodio "Isabella", Tony se hunde en un episodio depresivo severo y experimenta 
 alucinaciones: ve a una hermosa mujer italiana llamada Isabella en el jardín de su vecino. Tony ve a Isabella varias veces durante el episodio y más tarde se entera de que 
 ella nunca existió. Melfi teoriza que Isabella era una figura materna idealizada que el subconsciente de Tony produjo debido a que estaba profundamente molesto por las
- acciones de su propia madre en ese momento.
-
+acciones de su propia madre en ese momento.
 En "Sueño con Jeannie Cusamano", Tony termina abruptamente su terapia y convence al Dr. Melfi para que se esconda cuando descubre que su tío Junior se ha enterado de sus 
 sesiones.
-
 La relación entre Tony y el Dr. Melfi ha tenido altibajos, y Tony ha alcanzado un nivel de comodidad con el Dr. Melfi que nunca antes había experimentado con nadie más, ni 
 siquiera con su esposa. Esta cercanía lleva a Tony a tener una especie de "enamoramiento" del Dr. Melfi, algo que es inalcanzable. Sin embargo, el "entrometido" de la 
 Dra. Melfi es incómodo para Tony y a menudo se vuelve sarcástico y antagónico hacia ella, lo que lleva a una tensión continua en su relación. En el episodio en el que la
