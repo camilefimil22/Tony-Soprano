dee6c0d5f9fa3fbf6caad6048d2509234abc4191
camilefimil22 <camilalefimil@hotmail.com>
2025-05-30 21:40:59 -0300

    fotos de about

--- src/page/About.tsx
+++ src/page/About.tsx
@@ -1,7 +1,12 @@
-import starfield from '../assets/starfield-tony-soprano-1.jpg';
+import starfield from '../assets/starfield-tony-soprano-1.jpg'
 import famille from '../assets/la-famille-des-sopranos.jpg';
- 
- 
+import carmela from '../assets/carmela.jpg';
+import family from '../assets/the-sopranos-family-photo.webp';
+import habano from '../assets/habano.webp';
+import blundetto from '../assets/blundetto.jpg'
+
+
+
  export default function About(){
 return ( 
     <>
@@ -10,54 +15,64 @@ return (
     <div className="px-4 sm:px-6 lg:px-16 max-w-5xl mx-auto"> 
 
 <div className='flex gap-60 text-center  text-xl font-(family-name:--Roboto) text-black mx-auto mb-4'> 
-            Tony Soprano es miembro de la mafia italoamericana y, más adelante se convierte en el jefe de la familia criminal DiMeo de North Jersey. Muestra rasgos de comportamiento característicos de un sociópata violento, lucha contra la depresión y es propenso a los ataques de pánico. A partir del episodio piloto, busca tratamiento de la Dra. Jennifer Melfi y permanece en terapia de forma intermitente. </div>   
+     Tony Soprano es miembro de la mafia italoamericana y, más adelante se convierte en el jefe de la familia criminal DiMeo
+    de North Jersey. Muestra rasgos de comportamiento característicos de un sociópata violento, lucha contra la depresión 
+    y es propenso a los ataques de pánico. A partir del episodio piloto, busca tratamiento de la Dra. Jennifer Melfi y 
+    permanece en terapia de forma intermitente. </div>   
 
-    <img className= " bg-center h-full h-120 w-200 mx-auto mb-4" src= {famille}></img>
+    <img className= " bg-center h-120 w-200 mx-auto mb-4" src= {famille}></img>
     <div className="px-4 sm:px-6 lg:px-16 max-w-5xl mx-auto"/>
 
-<div className=" bg-[url(../assets/carmela.jpg)] bg-center h-100 w-150 mx-auto mb-4" />
-<div className=" bg-[url(../assets/the-sopranos-family-photo.webp)] bg-center h-90 w-150 mx-auto mb-4" />
-<div className='flex gap-60 text-center  text-xl font-(family-name:--Roboto) text-black'> 
-            Tony lucha por equilibrar las necesidades conflictivas de su familia (su esposa Carmela, su hija Meadow, su hijo A.J. y su madre Livia) con las de la mafia. 
-            Tony es un amante de los animales y disfruta alimentando a los patos que visitan su piscina. Tiene un apego sentimental 
-            hacia los animales, ya que había sido traumatizado por la pérdida de su perro de la infancia 
-            (como se reveló en el episodio "En Camelot"), cuyo nombre era "Tippy". Cuando va a confrontar a Angie Bonpensiero 
-            mientras pasea a su caniche, el perro saluda a Tony de manera amistosa, a lo que Tony corresponde. Durante la 
-            intervención de Christopher Moltisanti, cuando Tony escucha que Christopher asfixió accidentalmente al perro de 
-            Adrianna, se enfurece y dice: "¡Debería asfixiarte, pequeño imbécil!". </div>
-                
-<div className=" bg-[url(../assets/habano.webp)] bg-center h-190 w-170 mx-auto mb-4" />
-<div className='flex gap-60 text-center  text-xl font-(family-name:--Roboto) text-black'> 
-                
-            A lo largo de la serie, se muestra que Tony es un fumador frecuente de puros, así como un fumador ocasional de 
-            cigarrillos. Se le puede ver fumando un cigarro durante eventos importantes, como poco antes de que le informaran de 
-            la muerte de su madre y al deshacerse del cadáver de Ralph Cifaretto. En el episodio de la temporada 1 "A Hit Is a Hit", 
-            le da a su médico y vecino de al lado, Bruce Cusamano, una caja de cigarros cubanos como regalo de agradecimiento por referirlo 
-            al Dr. Melfi.
-            A Tony le gustan los deportes, especialmente el béisbol, el fútbol americano, el baloncesto, el golf y las carreras de 
-            caballos. Jugó béisbol y fútbol americano en West Essex High School, y es fanático de los Yankees de Nueva York y los Jets de 
-            Nueva York.
-</div>
+   <img className= " bg-center h-100 w-150 mx-auto mb-4" src= {carmela}></img>
+   <div className="px-4 sm:px-6 lg:px-16 max-w-5xl mx-auto" />
 
-<div className=" bg-[url(../assets/blundetto.jpg)] bg-center h-110 w-170 mx-auto mb-4" />
+    <img className= " bg-center h-90 w-150 mx-auto mb-4" src= {family}></img>
+    <div className="px-4 sm:px-6 lg:px-16 max-w-5xl mx-auto" />
+
+
 <div className='flex gap-60 text-center  text-xl font-(family-name:--Roboto) text-black'> 
-            Es un entusiasta de las armas y se muestra que tiene un arsenal en su casa. Regala armas de cumpleaños a su suegro Hugh
-             DeAngelis (en "Marco Polo") y recibe una de Bobby Baccialieri en su cumpleaños (en "Soprano Home Movies").
-            Tony mantiene un ávido interés en la historia, particularmente en la Segunda Guerra Mundial. A lo largo de las temporadas 4 y 5,
-             se ve a Tony viendo documentales sobre la guerra de Vietnam. Es un gran admirador del presidente John F. Kennedy y posee uno 
-             de sus sombreros de capitán marinero, que ganó en una subasta. A menudo se le muestra viendo programas en History Channel 
-             sobre líderes como George S. Patton, Erwin Rommel y Winston Churchill. Lee El arte de la guerra de Sun Tzu, que es citado por 
-             varios otros personajes de la serie, en particular Paulie Walnuts.
-            A menudo se ve a Tony viendo películas clásicas de la mafia. Por ejemplo, se le muestra viendo The Public Enemy (1931) a lo 
-            largo del episodio "Proshai, Livushka", que aborda la muerte de su madre. También comparte el amor por la serie de El Padrino, 
-            preguntándose qué salió mal en la tercera entrega, aunque él y sus amigos se entretienen mucho cuando Silvio hace una imitación
-             de Al Pacino a partir de ella.
-            Tony escucha música clásica, rock progresivo y pop, particularmente de las décadas de 1960 y 1970. En el transcurso del 
-            espectáculo se le ve disfrutar de AC/DC, Deep Purple, Eagles, Eric Clapton, Jefferson Airplane, Jethro Tull, Journey, Lynyrd 
-            Skynyrd, Pink Floyd, Rush, Steely Dan, The Clash, The Chi-Lites, The Lost Boys, Tom Petty and The Heartbreakers y Van Morrison.
-            A pesar de comentar sobre los peligros del uso de drogas para sus hijos y algunos de sus compañeros mafiosos, Tony se 
-            involucra en el uso recreativo varias veces en la segunda mitad de la serie, haciendo líneas de cocaína con Adriana, 
-            fumando marihuana y usando peyote en un viaje a Las Vegas.
+    Tony lucha por equilibrar las necesidades conflictivas de su familia (su esposa Carmela, su hija Meadow, su hijo A.J. y su madre Livia) con las de la mafia. 
+    Tony es un amante de los animales y disfruta alimentando a los patos que visitan su piscina. Tiene un apego sentimental 
+    hacia los animales, ya que había sido traumatizado por la pérdida de su perro de la infancia 
+    (como se reveló en el episodio "En Camelot"), cuyo nombre era "Tippy". Cuando va a confrontar a Angie Bonpensiero 
+    mientras pasea a su caniche, el perro saluda a Tony de manera amistosa, a lo que Tony corresponde. Durante la 
+    intervención de Christopher Moltisanti, cuando Tony escucha que Christopher asfixió accidentalmente al perro de 
+    Adrianna, se enfurece y dice: "¡Debería asfixiarte, pequeño imbécil!". </div>
+                
+    <img className= "bg-center h-190 w-170 mx-auto mb-4" src={habano}></img>
+    <div className=" px-4 sm:px-6 lg:px-16 max-w-5xl mx-auto" />
+
+<div className='flex gap-60 text-center  text-xl font-(family-name:--Roboto) text-black'> 
+     A lo largo de la serie, se muestra que Tony es un fumador frecuente de puros, así como un fumador ocasional de 
+    cigarrillos. Se le puede ver fumando un cigarro durante eventos importantes, como poco antes de que le informaran de 
+    la muerte de su madre y al deshacerse del cadáver de Ralph Cifaretto. En el episodio de la temporada 1 "A Hit Is a Hit", 
+    le da a su médico y vecino de al lado, Bruce Cusamano, una caja de cigarros cubanos como regalo de agradecimiento por referirlo 
+    al Dr. Melfi.
+    A Tony le gustan los deportes, especialmente el béisbol, el fútbol americano, el baloncesto, el golf y las carreras de 
+    caballos. Jugó béisbol y fútbol americano en West Essex High School, y es fanático de los Yankees de Nueva York y los Jets de 
+    Nueva York.</div>
+
+    <img className= "bg-center h-110 w-170 mx-auto mb-4" src={blundetto}></img>
+    <div className=" px-4 sm:px-6 lg:px-16 max-w-5xl mx-auto" />
+
+<div className='flex gap-60 text-center  text-xl font-(family-name:--Roboto) text-black'> 
+    Es un entusiasta de las armas y se muestra que tiene un arsenal en su casa. Regala armas de cumpleaños a su suegro Hugh
+    DeAngelis (en "Marco Polo") y recibe una de Bobby Baccialieri en su cumpleaños (en "Soprano Home Movies").
+    Tony mantiene un ávido interés en la historia, particularmente en la Segunda Guerra Mundial. A lo largo de las temporadas 4 y 5,
+    se ve a Tony viendo documentales sobre la guerra de Vietnam. Es un gran admirador del presidente John F. Kennedy y posee uno 
+    de sus sombreros de capitán marinero, que ganó en una subasta. A menudo se le muestra viendo programas en History Channel 
+    sobre líderes como George S. Patton, Erwin Rommel y Winston Churchill. Lee El arte de la guerra de Sun Tzu, que es citado por 
+    varios otros personajes de la serie, en particular Paulie Walnuts.
+    A menudo se ve a Tony viendo películas clásicas de la mafia. Por ejemplo, se le muestra viendo The Public Enemy (1931) a lo 
+    largo del episodio "Proshai, Livushka", que aborda la muerte de su madre. También comparte el amor por la serie de El Padrino, 
+    preguntándose qué salió mal en la tercera entrega, aunque él y sus amigos se entretienen mucho cuando Silvio hace una imitación
+    de Al Pacino a partir de ella.
+    Tony escucha música clásica, rock progresivo y pop, particularmente de las décadas de 1960 y 1970. En el transcurso del 
+    espectáculo se le ve disfrutar de AC/DC, Deep Purple, Eagles, Eric Clapton, Jefferson Airplane, Jethro Tull, Journey, Lynyrd 
+    Skynyrd, Pink Floyd, Rush, Steely Dan, The Clash, The Chi-Lites, The Lost Boys, Tom Petty and The Heartbreakers y Van Morrison.
+    A pesar de comentar sobre los peligros del uso de drogas para sus hijos y algunos de sus compañeros mafiosos, Tony se 
+    involucra en el uso recreativo varias veces en la segunda mitad de la serie, haciendo líneas de cocaína con Adriana, 
+    fumando marihuana y usando peyote en un viaje a Las Vegas.
 </div>  
     </div>
     </>
